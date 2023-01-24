49a60a854064171b8da69a6d2d3d5db446327404
“Omar <ocamacho812@gmail.com>
2023-01-24 01:05:48 -0800

    refactor phase 4 problem

--- phase_4_recursion.js
+++ phase_4_recursion.js
@@ -1,7 +1,7 @@
 function range(start, end) {
-  if (start === end) return [start];
-
-  return start.concat(range(start + 1, end));
+  return start === end
+    ? [start]
+    : start.concat(range(start + 1, end));
 }
 
 const sumRec = function (arr) {
